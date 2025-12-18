3528997aa88ed9b6b7392a27d66d105445f7e960
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-12-18 19:12:24 +0700

    Menambahlan HostNavigasi dan DataSiswaApp ke PetaNavigasi.kt

---
+++ app/src/main/java/com/example/questapi_063/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,38 @@
+package com.example.questapi_063.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questapi_063.View.EntrySiswaScreen
+import com.example.questapi_063.View.HomeScreen
+import com.example.questapi_063.uicontroller.route.DestinasiEntry
+import com.example.questapi_063.uicontroller.route.DestinasiHome
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier
+){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    // navController.navigate("${DestinasiDetail.route}/$it")
+                })
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
