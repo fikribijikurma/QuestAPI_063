90f75fe4601f97a85c053bcd4d33c1f479db0f63
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-12-25 11:55:25 +0700

    Perbarui antarmuka RepositoryDataSiswa dan ganti nama implementasinya menjadi JaringanRepositoryDataSiswa.

--- app/src/main/java/com/example/questapi_063/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_063/repositori/RepositoryDataSiswa.kt
@@ -4,17 +4,28 @@ import com.example.questapi_063.apiservice.ServiceApiSiswa
 import com.example.questapi_063.modeldata.DataSiswa
 
 interface RepositoryDataSiswa {
-    suspend fun getDataSiswa(): List<DataSiswa>
-    suspend fun PostDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int) : DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int) : retrofit2.Response<Void>
 }
 
-class RepositoryDataSiswaImpl(private val serviceApiSiswa: ServiceApiSiswa
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
 ) : RepositoryDataSiswa {
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa
-        .getSiswa()
-    override suspend fun PostDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> =
-        serviceApiSiswa.insertSiswa(dataSiswa)
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
 
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.postSiswa(dataSiswa)
+
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
 }
 
 
