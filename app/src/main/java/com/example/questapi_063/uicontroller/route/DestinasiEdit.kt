ca2fb504b38a51e73c352f12fc44bb17d85d15c5
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-12-25 12:38:54 +0700

    Tambahkan objek DestinasiEdit ke DestinasiEdit.kt

---
+++ app/src/main/java/com/example/questapi_063/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,10 @@
+package com.example.questapi_063.uicontroller.route
+
+import com.example.questapi_063.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
