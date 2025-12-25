929aefa25f0ad0be1fc8fd6f97e642e87e591914
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-12-25 12:19:44 +0700

    Menambahkan pengkondisian untuk status Loading, Error, dan Success.

--- app/src/main/java/com/example/questapi_063/View/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_063/View/HalamanDetail.kt
@@ -1,5 +1,36 @@
 package com.example.questapi_063.View
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_063.R
+import com.example.questapi_063.Viewmodel.DetailViewModel
+import com.example.questapi_063.Viewmodel.StatusUIDetail
+import com.example.questapi_063.Viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailSiswaScreen(
@@ -48,5 +79,50 @@ fun DetailSiswaScreen(
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
         )
+    }
+}
+
+@Composable
+private fun BodyDetailSiswa(
+    statusUIDetail: StatusUIDetail,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+        when (statusUIDetail) {
+            is StatusUIDetail.Success -> {
+                ItemDetailSiswa(
+                    siswa = statusUIDetail.satusiswa,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedButton(
+                    onClick = { deleteConfirmationRequired = true },
+                    shape = MaterialTheme.shapes.small,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(stringResource(R.string.delete))
+                }
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDelete()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+                    )
+                }
+            }
+            is StatusUIDetail.Loading -> {
+                Text(text = "Loading...")
+            }
+            is StatusUIDetail.Error -> {
+                Text(text = "Error loading data")
+            }
+        }
     }
 }
