d580694bc4c47011cc4e1776f4a1bd85c5ff0a15
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-12-25 12:22:01 +0700

    Membuat komponen Row yang dapat digunakan kembali untuk menampilkan label dan data.

--- app/src/main/java/com/example/questapi_063/View/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_063/View/HalamanDetail.kt
@@ -1,7 +1,10 @@
 package com.example.questapi_063.View
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -26,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questapi_063.R
 import com.example.questapi_063.Viewmodel.DetailViewModel
@@ -159,5 +163,16 @@ fun ItemDetailSiswa(
                 itemDetail = siswa.telpon,
             )
         }
+    }
+}
+
+@Composable
+private fun ItemDetailData(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
     }
 }
