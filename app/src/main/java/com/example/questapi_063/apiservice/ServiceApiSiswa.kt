05e9b23494c5d3abc04b8839a8e71b8f5da4cfe4
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-12-25 11:51:16 +0700

    Perbarui antarmuka ServiceApiSiswa dengan operasi CRUD dan perubahan endpoint

--- app/src/main/java/com/example/questapi_063/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_063/apiservice/ServiceApiSiswa.kt
@@ -2,16 +2,26 @@ package com.example.questapi_063.apiservice
 
 import com.example.questapi_063.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
-    @POST("insertTeman.php")
-    suspend fun insertSiswa(
-        @Body dataSiswa: DataSiswa
-    ):retrofit2.Response<Void>
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int): retrofit2.Response<Void>
 }
 
